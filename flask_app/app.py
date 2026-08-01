a066cd05fe7fefbd7aaabde234f94902edb7b19f
hyuda890 <yudai77382@gmail.com>
2026-08-01 17:27:41 +0900

    ログイン実装

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask
+from flask_migrate import Migrate
+
+from controllers.main_controller import main
+from models import db
+
+base_dir = os.path.dirname(__file__)
 
 app = Flask(__name__)
+app.secret_key = "dev-secret-key-change-me"
 
-@app.route("/")
-def index():
-  return render_template("index.html", title="トップページ")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(base_dir, "data.sqlite")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+with app.app_context():
+  db.create_all()
+
+app.register_blueprint(main)
 
 if __name__ == "__main__":
   app.run(debug=True)
